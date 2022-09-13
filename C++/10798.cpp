4e8052a5a9a4f9a18e76740f0761c0082049715a
lsuinl <su96in43@naver.com>
2022-09-13 22:07:46 +0900

    2022.09.13

--- C++/10798.cpp
+++ C++/10798.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(){
+    string suin[5];
+    for(int i=0;i<5;i++){
+        cin>>suin[i];
+    }
+
+    for(int i=0;i<15;i++){
+        for(int j=0;j<5;j++){
+            if(i<suin[j].length())
+                cout<<suin[j].at(i);
+        }
+    }
 
     return 0;
 }
