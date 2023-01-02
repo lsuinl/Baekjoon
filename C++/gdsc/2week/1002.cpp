1d94d7e63a63389c470b35a80e9d7348337b353e
lsuinl <su96in43@naver.com>
2023-01-02 16:45:22 +0900

    22.01.02

--- C++/gdsc/2week/1002.cpp
+++ C++/gdsc/2week/1002.cpp
@@ -2,7 +2,124 @@
 
 using namespace std;
 
-int main(){
-    int T,
+int main()
+{
+    int T, x1, y1, r1, x2, y2, r2;
+    cin >> T;
+    for (int i = 0; i < T; i++)
+    {
+        int result = 0;
+        cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+        if (x1 == x2 && y1 == y2 && r1 == r2)
+        { // 무한대인경우 -1
+            cout << -1 << endl;
+            continue;
+        }
+        // 가장 끝 꼭짓점 4개 저장1번원
+        int square2[4][2] = {{x2 - r2, y2}, {x2, y2 + r2}, {x2 + r2, y2}, {x2, y2 - r2}};
+        ; // 2번원
+        for (int j = 0; j < 4; j++)
+        {
+            int x = square2[j][0]; // 2번원의 꼭짓점
+            int y = square2[j][1];
+            if (x >= x1 - r1 && x <= x1 &&
+                y >= y1 && y <= y1 + r1)
+            {
+                for (int z = 0; z < r1 + 1; z++)
+                { // 해당 면의 1번원의 선과 꼭짓점과의 관계찾기
+                    int xx = x1 - z;
+                    int yy = y1 + (r1 - z);
+                    if (xx == x && yy == y)
+                    { // 선에 위치.
+                        result++;
+                        break;
+                    }
+                    else if (xx > x && yy < y)
+                    { // 범위밖
+                        break;
+                    }
+                    if (z == r1)
+                    { // 범위 안
+                        result = 2;
+                    }
+                }
+            }
+            else if (x >= x1 && x <= x1 + r1 &&
+                    y >= y1 && y <= y1 + r1)
+            {
+                for (int z = 0; z < r1 + 1; z++)
+                {
+                    int xx = x1 + z;
+                    int yy = y1 + (r1 - z);
+                    if (xx == x && yy == y)
+                    { // 선에 위치.
+                        result++;
+                        break;
+                    }
+                    else if (xx < x && yy < y)
+                    { // 범위밖
+                        break;
+                    }
+
+                    if (z == r1)
+                    { // 범위 안
+                        result = 2;
+                    }
+                }
+            }
+            else if (x >= x1 - r1 && x <= x1 &&
+                    y >= y1 - r1 && y <= y1)
+            {
+                for (int z = 0; z < r1 + 1; z++)
+                {
+                    int xx = x1 - z;
+                    int yy = y1 - (r1 + z);
+                    if (xx == x && yy == y)
+                    { // 선에 위치.
+                        result++;
+                        break;
+                    }
+                    else if (xx > x && yy > y)
+                    { // 범위밖
+                        break;
+                    }
+
+                    if (z == r1)
+                    { // 범위 안
+                        result = 2;
+                    }
+                }
+            }
+            else if (x >= x1 && x <= x1 + r1 &&
+                    y >= y1 - r1 && y <= y1)
+            {
+                for (int z = 0; z < r1 + 1; z++)
+                {
+                    int xx = x1 + z;
+                    int yy = y1 - (r1 + z);
+                    if (xx == x && yy == y)
+                    { // 선에 위치.
+                        result++;
+                        break;
+                    }
+                    else if (xx < x && yy > y)
+                    { // 범위밖
+                        break;
+                    }
+
+                    if (z == r1)
+                    { // 범위 안
+                        result = 2;
+                    }
+                }
+            }
+            if (result > 3)
+            {
+                result = 0;
+                break;
+            }
+        }
+        cout << result << endl;
+    }
     return 0;
 }
--- C++/gdsc/2week/9020.cpp
+++ C++/gdsc/2week/9020.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
+int main()
+{
+    int T;
+    cin >> T;
+    int suin[10001];
+    fill(suin, suin+10001,false);
 
+    for (int i = 2; i <= 10000; i++)
+    {
+        int check = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % j == 0)
+            { // 소수제크
+                check++;
+            }
+        }
+        if (check == 2) // check==2면 소수인거시다.
+            suin[i] = true;
+    }
+    for (int i = 0; i < T; i++)
+    {
+        int n, p1, p2;
+        cin >> n;
+        for (int j = n / 2; j < n;j++)
+        {
+            if (suin[j] && suin[n - j])
+            { // 둘다소수면.
+                p2 = j;
+                p1 = n - j;
+                break;
+            }
+        }
+        cout << p1 << " " << p2 << endl;
+    }
     return 0;
 }
