5137d448017c08d367a61743e121c7d21c45461f
lsuinl <su96in43@naver.com>
2023-01-30 14:58:25 +0900

    22.01.30

--- C++/inflearn/3week/3.cpp
+++ C++/inflearn/3week/3.cpp
@@ -1,8 +1,77 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// int n;
+
+// class people{
+//     public:
+//     char name;
+//     int stat;
+// };
+
+// int point(people suin[],int number){
+//     char name=suin[number].name;
+//     int stat=suin[number].stat;
+//     int result=0;
+//     for(int i=0;i<n;i++){
+//         if(name!=suin[i].name){
+//             if(stat>suin[i].stat){
+//                 result+=suin[i].stat;
+//             }
+//         }
+//     }
+
+//     return result;
+// }
+
+// int main(){
+//     cin>>n;
+//     people suin[n];
+//     for(int i=0;i<n;i++){
+//         cin>>suin[i].name>>suin[i].stat;
+//     }
+
+//     for(int i=0;i<n;i++){
+//         cout<<point(suin, i)<<'\n';
+//     }
+//     return 0;
+// }
+
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
     
+    int n;
+    cin>>n;
+    
+    multimap<char, int> suin;
+    char a[n];
+    int b[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i]>>b[i];
+        suin.insert(pair<char,int>(a[i],b[i]));
+    }
+    for(int i=0;i<n;i++){
+        int name=a[i];
+        int stat=b[i];
+        int result=0;
+        multimap<char,int>::iterator checkdown=suin.lower_bound(name);
+        multimap<char,int>::iterator checkup=suin.upper_bound(name);
+        multimap<char,int>::iterator check;
+        for(check=suin.begin();check!=checkdown;check++)
+            if(check->second<stat)  result+=check->second;
+        for(check=checkup;check!=suin.end();check++)
+            if(check->second<stat)  result+=check->second;
+        cout<<result<<"\n";
+    }
     return 0;
 }
---
+++ C++/inflearn/3week/3ans.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+struct Student{
+	int n, p;
+	char t;
+	Student(int a, char b, int c){
+		n=a;
+		t=b;
+		p=c;
+	}
+	bool operator<(const Student &b)const{
+		return p<b.p;
+	}
+};
+int main(){
+	ios_base::sync_with_stdio(false);
+	int n;
+	long long bsum=0;
+	cin>>n;
+	vector<int> res(n);
+	unordered_map<char, int> team;
+	vector<Student> st;
+	for(int i=0; i<n; i++){
+		char a;
+		int b;
+		cin>>a>>b;
+		st.push_back(Student(i, a, b));
+	}
+	sort(st.begin(), st.end());
+	int j=0;
+	for(int i=1; i<n; i++){
+		for( ; j<n; j++){
+			if(st[i].p>st[j].p){
+				bsum+=st[j].p;
+				team[st[j].t]+=st[j].p;
+			}
+			else break;
+		}
+		res[st[i].n]=bsum-team[st[i].t];
+	}
+	for(int i=0; i<n; i++){
+		cout<<res[i]<<"\n";
+	}
+	return 0;
+}
