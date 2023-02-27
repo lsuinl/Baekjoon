315b4ff7231bd0292a77f2b6128e032d1b8d0922
lsuinl <su96in43@naver.com>
2023-02-27 16:14:22 +0900

    docs: 주석변경

--- C++/gdsc/9week/1009.cpp
+++ C++/gdsc/9week/1009.cpp
@@ -2,19 +2,27 @@
 #include <math.h>
 using namespace std;
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
     int t;
-    cin>>t;
-    for(int o=0;o<t;o++){
-        long long a,b;
-        cin>>a>>b;
-        b=(b%4)+4;
-        a=(long long)(pow(a,b))%10;
-        if(a==0){a=10;}
-        cout<<a<<"\n";
+    cin >> t;
+
+    for (int o = 0; o < t; o++)
+    {
+        long long a, b;
+        cin >> a >> b;
+			//거듭제곱 : 4번마다 끝자리가 반복되어 나타남
+        b = (b % 4) + 4; 
+        a = (long long)(pow(a, b)) % 10;
+        if (a == 0) //나머지가 0인 경우에는 10으로 판단
+        {
+            a = 10;
+        }
+        cout << a << "\n";
     }
     return 0;
-} 
+}
--- C++/gdsc/9week/1049.cpp
+++ C++/gdsc/9week/1049.cpp
@@ -2,23 +2,27 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int n,m,result=0;
-    int minbundle=6006,minsingle=1001;
-    cin>>n>>m;
+
+    int n, m, result = 0;
+    int minbundle = 6006, minsingle = 1001;
+
+    cin >> n >> m;
     int bundle[m], single[m];
-    for(int i=0;i<m;i++){
-        cin>>bundle[i]>>single[i];
-        bundle[i]=min(bundle[i],single[i]*6);
-        minbundle=bundle[i]<minbundle ? bundle[i]:minbundle;
-        minsingle=single[i]>minsingle ? minsingle:single[i];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> bundle[i] >> single[i];
+        bundle[i] = min(bundle[i], single[i] * 6); //번들과 6개 낱개 중 저렴한 값 선택
+        minbundle = bundle[i] < minbundle ? bundle[i] : minbundle; //번들의 최솟값 저장
+        minsingle = single[i] > minsingle ? minsingle : single[i]; //낱개의 최솟값 저장
     }
-    result+=(n/6)*minbundle;
-    n=n%6;
-    result+=(n*minsingle>minbundle?minbundle:minsingle*n);
-    cout<<result;
+    result += (n / 6) * minbundle; //6개 번들로 살 수 있는 개수만큼 더하기
+    n = n % 6; //6개미만
+    result += (n * minsingle > minbundle ? minbundle : minsingle * n);
+    cout << result;
     return 0;
 }
