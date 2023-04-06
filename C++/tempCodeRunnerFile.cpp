c09d983306b641a84a20d7e5b324615f135a5d66
lsuinl <su96in43@naver.com>
2023-04-07 00:38:53 +0900

    23.04.07

---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>m>>n;
+    char board[51][51];
+    int count, min=100;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>board[i][j];
+        }
+    }
+
+    for(int a=0;a<=m-8;a++){
+        for(int b=0;b<=n-8;b++){
+            count=0;
+            for(int i=a;i<a+8;i++){
+                for(int j=b;j<b+8;j++){
+                    if(i%2==0){
+                        if(j%2==0)   count=board[i][j]=='W' ? count+1:count; //짝-짝=흰색
+                        else    count=board[i][j]=='B' ? count+1:count; //짝-홀=검은색
+                    }
+                    else{
+                        if(j%2==0) count=board[i][j]=='B' ? count+1:count; //홀-짝=검은색
+                        else count=board[i][j]=='W' ? count+1:count; //짝-홀=흰색
+                    }
+                }
+            }
+count=board[a][b]=='B'? (64-count): count;
+if(min>count) min = count;
+        }
+    }
+    cout<<min;
+    return 0;
+}
