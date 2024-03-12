56a931537e972ac4920a28aec507d291e4a4cac3
SUIN <87413903+lsuinl@users.noreply.github.com>
2024-03-12 11:21:36 +0900

    1874 복습
    문제를 더 상세하게 보고 규칙을 찾을 것.

--- C++/1874.cpp
+++ C++/1874.cpp
@@ -1,37 +1,49 @@
-#include<iostream>
-#include<vector>
-#include<stack>
+#include <iostream>
+#include <stack>
+#include <vector>
 using namespace std;
 
-int main(void)
-{
-	vector<char>output;
-	stack<int>seq;
-	int n;
-	int cnt = 1;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		int num;
-		cin >> num;
+//한번 넣으면 빠져나올 수 없다!
 
-		while (cnt <= num)
-		{
-			seq.push(cnt);
-			cnt++;
-			output.push_back('+');
-		}
-		if (seq.top() == num)
-		{
-			seq.pop();
-			output.push_back('-');
-		}
-		else
-		{
-			cout << "NO";
-			return 0;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	stack<int> stacks;
+	vector<char> answer;
+	bool no = false;
+	int max=1;
+	int n; cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int a; cin >> a;
+		while(1){
+			if(stacks.empty()){
+				answer.push_back('+');
+				stacks.push(max);
+				max++;
+			}
+			
+			if(stacks.top()==a){
+				answer.push_back('-');
+				stacks.pop();	
+				break;
+			}
+			else if (stacks.top()>a) {
+				no = true;
+				break;
+			}
+			else{
+				answer.push_back('+');
+				stacks.push(max);	
+				max++;
+			}
 		}
 	}
-	for (int i = 0; i < output.size(); i++)
-		cout << output[i] << '\n';
+	
+	if (no) cout << "NO";
+	else for (int i = 0; i < answer.size(); i++) cout << answer[i] << "\n";
+
+	return 0;
 }
