aee251dd6786c5e26bf0445661dacb243967925a
SUIN <87413903+lsuinl@users.noreply.github.com>
2024-03-29 12:23:25 +0900

    수정

--- C++/2751.cpp
+++ C++/2751.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int n;
     int num[1000000];   
+    cin>>n;
     for(int i=0;i<n;i++)
         cin>>num[i];
     sort(num,num+n);
@@ -12,4 +13,4 @@ int main(){
         cout<<num[i]<<"\n";
     
    return 0;
-}
+} 
