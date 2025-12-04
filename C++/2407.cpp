ad8016b31cee9443b309c4a4042638a6d95891fd
lsuinl <su96in43@naver.com>
2025-12-04 15:23:02 +0900

    25.12.04

--- C++/2407.cpp
+++ C++/2407.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
@@ -6,17 +7,34 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
     int j=1;
     int n,m;
-    long long result=1;
+    __uint128_t result=1, nq=1;
     cin>>n>>m;
-    for(int i=(n-m);i<=n;i++){
+    for(int i=(n-m)+1;i<=n;i++){
         result*=i;
-        if(result/j>1 && j<=m){
-            result=result/j;
-            j++;
+        if(result%j==0){
+            result/=j;
         }
+        else{
+            nq*=j;
+        }
+        if(nq!=1 &&result%nq==0){
+            result/=nq;
+            nq=1;
+        }
+        j++;
     }
-    cout<<result;
+    result/=nq;
+    string s;
+    if(result == 0)
+        cout << 0;
+    while(result>0){
+        int num = result%10;
+        s.insert(s.begin(),'0'+num);
+        result/=10;
+    }
+    cout<<s;
     return 0;
 }
